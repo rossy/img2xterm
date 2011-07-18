ce45cc3a85efc6dbd409a90fc43ed5b5b3f822ed
rossy2401 <rossymiles@optusnet.com.au>
2011-07-18 23:04:52 +1000

    For the sake of feature bloat, added colorspace conversion and manual pages.

--- Makefile
+++ Makefile
@@ -1,18 +1,29 @@
 CFLAGS = -O2 -Wall
 LDFLAGS = -s
-LIBS = -lncurses
+LIBS = -lm -lncurses
 PREFIX = /usr/local
+
+all: img2xterm man6/img2xterm.6.gz
 
 img2xterm: img2xterm.c
 	$(CC) $(CFLAGS) $(LDFLAGS) $(LIBS) -o $@ $< `MagickWand-config --cflags --ldflags --libs`
 
+man6:
+	mkdir man6
+
+man6/img2xterm.6.gz: man6/img2xterm.6 | man6
+	gzip -fc $< > $@
+
+man6/img2xterm.6: img2xterm.c | man6
+	help2man -s 6 -N -m " " --version-string="git" ./img2xterm -o $@
+
 .PHONY: all install clean
 
-all: img2xterm
-
 install: img2xterm
-	install -m 0755 img2xterm $(PREFIX)/bin
+	install -D -m 0755 img2xterm $(PREFIX)/bin/img2xterm
 	ln -fs $(PREFIX)/bin/img2xterm $(PREFIX)/bin/img2cow
+	install -D -m 0644 man6/img2xterm.6.gz $(PREFIX)/share/man/man6/img2xterm.6.gz
+	ln -fs $(PREFIX)/share/man/man6/img2xterm.6.gz $(PREFIX)/share/man/man6/img2cow.6.gz
 
 clean:
-	-rm -f img2xterm
+	-rm -f img2xterm man6/img2xterm.6 man6/img2xterm.6.gz
--- img2xterm.c
+++ img2xterm.c
@@ -30,6 +30,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
+#include <math.h>
 #include <ImageMagick/wand/MagickWand.h>
 #ifndef NO_CURSES
 #include <term.h>
@@ -41,9 +42,12 @@ enum {
 };
 
 unsigned char* colortable;
+double* labtable;
 const unsigned char valuerange[] = { 0x00, 0x5F, 0x87, 0xAF, 0xD7, 0xFF };
 unsigned long oldfg = color_undef;
 unsigned long oldbg = color_undef;
+int perceptive = 0;
+double chroma_weight = 1.0;
 
 #ifndef NO_CURSES
 int use_terminfo = 0;
@@ -51,6 +55,70 @@ char* ti_setb;
 char* ti_setf;
 char* ti_op;
 #endif
+
+void srgb2lab(unsigned char red, unsigned char green, unsigned char blue,
+	double* lightness, double* aa, double* bb)
+{
+	double r = (double)red / 255.0;
+	double g = (double)green / 255.0;
+	double b = (double)blue / 255.0;
+	
+	double rl = r <= 0.4045 ? r / 12.92 : pow((r + 0.055) / 1.055, 2.4);
+	double gl = g <= 0.4045 ? g / 12.92 : pow((g + 0.055) / 1.055, 2.4);
+	double bl = b <= 0.4045 ? b / 12.92 : pow((b + 0.055) / 1.055, 2.4);
+	
+	double x = 0.4124564 * rl + 0.3575761 * gl + 0.1804375 * bl;
+	double y = 0.2126729 * rl + 0.7151522 * gl + 0.0721750 * bl;
+	double z = 0.0193339 * rl + 0.1191920 * gl + 0.9503041 * bl;
+	
+	double xn = x / 0.95047;
+	double yn = y;
+	double zn = z / 1.08883;
+	
+	double fxn = xn > (216.0 / 24389.0) ? pow(xn, 1.0 / 3.0)
+		: (841.0 / 108.0) * xn + (4.0 / 29.0);
+	double fyn = yn > (216.0 / 24389.0) ? pow(yn, 1.0 / 3.0)
+		: (841.0 / 108.0) * yn + (4.0 / 29.0);
+	double fzn = zn > (216.0 / 24389.0) ? pow(zn, 1.0 / 3.0)
+		: (841.0 / 108.0) * zn + (4.0 / 29.0);
+	
+	*lightness = 116.0 * fyn - 16.0;
+	*aa = (500.0 * (fxn - fyn)) * chroma_weight;
+	*bb = (200.0 * (fyn - fzn)) * chroma_weight;
+}
+
+void srgb2yiq(unsigned char red, unsigned char green, unsigned char blue,
+	double* y, double* i, double* q)
+{
+	double r = (double)red / 255.0;
+	double g = (double)green / 255.0;
+	double b = (double)blue / 255.0;
+	
+	*y =   0.299    * r +  0.587    * g +  0.144    * b;
+	*i =  (0.595716 * r + -0.274453 * g + -0.321263 * b) * chroma_weight;
+	*q =  (0.211456 * r + -0.522591 * g +  0.311135 * b) * chroma_weight;
+}
+
+double cie94(double l1, double a1, double b1, double l2, double a2, double b2)
+{
+	const double kl = 1;
+	const double k1 = 0.045;
+	const double k2 = 0.015;
+	
+	double c1 = sqrt(a1 * a1 + b1 * b1);
+	double c2 = sqrt(a2 * a2 + b2 * b2);
+	double dl = l1 - l2;
+	double dc = c1 - c2;
+	double da = a1 - a2;
+	double db = b1 - b2;
+	double dh = sqrt(da * da + db * db - dc * dc);
+	
+	double t1 = dl / kl;
+	double t2 = dc / (1 + k1 * c1);
+	double t3 = dh / (1 + k2 * c1);
+	
+	return sqrt(t1 * t1 + t2 * t2 + t3 * t3);
+}
 
 void xterm2rgb(unsigned char color, unsigned char* rgb)
 {
@@ -63,6 +131,53 @@ void xterm2rgb(unsigned char color, unsigned char* rgb)
 	}
 	else
 		rgb[0] = rgb[1] = rgb[2] = 8 + (color - 232) * 10;
+}
+
+unsigned long rgb2xterm_cie94(unsigned char r, unsigned char g,
+	unsigned char b)
+{
+	unsigned long i = 16, ret = 0;
+	double d, smallest_distance = INFINITY;
+	double l, aa, bb;
+	
+	srgb2lab(r, g, b, &l, &aa, &bb);
+	
+	for (; i < 256; i++)
+	{
+		d = cie94(l, aa, bb, labtable[i * 3], labtable[i * 3 + 1],
+			labtable[i * 3 + 2]);
+		if (d < smallest_distance)
+		{
+			smallest_distance = d;
+			ret = i;
+		}
+	}
+	
+	return ret;
+}
+
+unsigned long rgb2xterm_yiq(unsigned char r, unsigned char g,
+	unsigned char b)
+{
+	unsigned long i = 16, ret = 0;
+	double d, smallest_distance = INFINITY;
+	double y, ii, q;
+	
+	srgb2yiq(r, g, b, &y, &ii, &q);
+	
+	for (; i < 256; i++)
+	{
+		d = (y - labtable[i * 3]) * (y - labtable[i * 3]) + 
+			(ii - labtable[i * 3 + 1]) * (ii - labtable[i * 3 + 1]) + 
+			(q - labtable[i * 3 + 2]) * (q - labtable[i * 3 + 2]);
+		if (d < smallest_distance)
+		{
+			smallest_distance = d;
+			ret = i;
+		}
+	}
+	
+	return ret;
 }
 
 unsigned long rgb2xterm(unsigned char r, unsigned char g, unsigned char b)
@@ -159,9 +274,26 @@ unsigned long fillrow(PixelWand** pixels, unsigned long* row,
 			row[i] = color_transparent;
 		else
 		{
-			row[i] = rgb2xterm((unsigned long)(PixelGetRed(pixels[i]) * 255.0),
-				(unsigned long)(PixelGetGreen(pixels[i]) * 255.0),
-				(unsigned long)(PixelGetBlue(pixels[i]) * 255.0));
+			switch (perceptive)
+			{
+				case 0:
+					row[i] = rgb2xterm(
+						(unsigned long)(PixelGetRed(pixels[i]) * 255.0),
+						(unsigned long)(PixelGetGreen(pixels[i]) * 255.0),
+						(unsigned long)(PixelGetBlue(pixels[i]) * 255.0));
+					break;
+				case 1:
+					row[i] = rgb2xterm_cie94(
+						(unsigned long)(PixelGetRed(pixels[i]) * 255.0),
+						(unsigned long)(PixelGetGreen(pixels[i]) * 255.0),
+						(unsigned long)(PixelGetBlue(pixels[i]) * 255.0));
+					break;
+				case 2:
+					row[i] = rgb2xterm_yiq(
+						(unsigned long)(PixelGetRed(pixels[i]) * 255.0),
+						(unsigned long)(PixelGetGreen(pixels[i]) * 255.0),
+						(unsigned long)(PixelGetBlue(pixels[i]) * 255.0));
+			}
 			lastpx = i;
 		}
 	}
@@ -172,8 +304,7 @@ unsigned long fillrow(PixelWand** pixels, unsigned long* row,
 void usage(int ret, char* binname)
 {
 	fprintf(ret ? stderr: stdout,
-		"Usage: %s [ -chi ] [ -l stem-length ] [ -m margin ] [ -s stem-margin "
-		"]\n    [ infile ] [ outfile ]\n"
+		"Usage: %s [ options ] [ infile ] [ outfile ]\n\n"
 		"Convert bitmap images to 256 color block elements suitable for"
 		" display in xterm\nand similar terminal emulators.\n\n"
 		"Options:\n"
@@ -185,13 +316,38 @@ void usage(int ret, char* binname)
 		"  -i, --terminfo              use terminfo to set the colors of each"
 		" block\n"
 #endif
-		"  -l, --stem-length <length>  length of the speech bubble stem when"
+		"  -l, --stem-length [length]  length of the speech bubble stem when"
 		" generating\n"
 		"                              cowfiles (default: 4)\n"
-		"  -m, --margin <width>        add a margin to the left of the image\n"
-		"  -s, --stem-margin <width>   margin for the speech bubble stem when"
+		"  -m, --margin [width]        add a margin to the left of the image\n"
+		"  -p, --perceptive            use the CIE94 color difference formula"
+		" for color\n"
+		"                              conversion instead of simple RGB linear"
+		" distance\n"
+		"  -s, --stem-margin [width]   margin for the speech bubble stem when"
 		" generating\n"
 		"                              cowfiles (default: 11)\n"
+		"  -w, --chroma-weight [mult]  weighting for chroma in --perceptive"
+		" and --yiq\n"
+		"                              modes (default: 1)\n"
+		"  -y, --yiq                   use linear distance in the YIQ"
+		" color space\n"
+		"                              instead of RGB for color conversion\n"
+		"\nIf the output file name is omitted the image is written to stdout"
+		". If both the\ninput file name and the output file name are missing"
+		", img2xterm will act as a\nfilter.\n\n"
+		"Examples:\n"
+		"  img2xterm -yw 2 image.png   display a bitmap image on the"
+		" terminal using the\n"
+		"                              YIQ color space for conversion with"
+		" weighted\n"
+		"                              chroma\n"
+		"  img2xterm banner.png motd   save a bitmap image as a text file to"
+		" display\n"
+		"                              later\n"
+		"  img2cow rms.png rms.cow     create a cowfile (assuming img2cow is a"
+		" link to\n"
+		"                              img2xterm)\n"
 		, binname);
 	exit(ret);
 }
@@ -228,6 +384,8 @@ int main(int argc, char** argv)
 							if (!*++argv || !sscanf(*argv, "%lu", &stemlen))
 								usage(1, binname);
 						}
+						else if (!strcmp("perceptive", *argv))
+							perceptive = 1;
 						else if (!strcmp("margin", *argv))
 						{
 							if (!*++argv || !sscanf(*argv, "%lu", &margin))
@@ -242,6 +400,14 @@ int main(int argc, char** argv)
 						else if (!strcmp("terminfo", *argv))
 							use_terminfo = 1;
 #endif
+						else if (!strcmp("chroma-weight", *argv))
+						{
+							if (!*++argv || !sscanf(*argv, "%lf",
+								&chroma_weight))
+								usage(1, binname);
+						}
+						else if (!strcmp("yiq", *argv))
+							perceptive = 2;
 						else
 						{
 							fprintf(stderr,
@@ -271,11 +437,22 @@ int main(int argc, char** argv)
 							&margin))
 							usage(1, binname);
 						goto nextarg;
+					case 'p':
+						perceptive = 1;
+						break;
 					case 's':
 						if (*++*argv || !*++argv || !sscanf(*argv, "%lu",
 							&stemmargin))
 							usage(1, binname);
 						goto nextarg;
+					case 'w':
+						if (*++*argv || !*++argv || !sscanf(*argv, "%lf",
+							&chroma_weight))
+							usage(1, binname);
+						goto nextarg;
+					case 'y':
+						perceptive = 2;
+						break;
 					default:
 						fprintf(stderr, "%s: unrecognised option -%c",
 							binname, *--*argv);
@@ -337,10 +514,30 @@ int main(int argc, char** argv)
 			return 2;
 		}
 	
-	colortable = malloc(256 * 3 * sizeof(unsigned char));
-	for (i = 16; i < 256; i ++)
-		xterm2rgb(i, colortable + i * 3);
-	
+	if (perceptive)
+	{
+		unsigned char rgb[3];
+		double l, a, b;
+		
+		labtable = malloc(256 * 3 * sizeof(double));
+		for (i = 16; i < 256; i ++)
+		{
+			xterm2rgb(i, rgb);
+			if (perceptive == 1)
+				srgb2lab(rgb[0], rgb[1], rgb[2], &l, &a, &b);
+			else
+				srgb2yiq(rgb[0], rgb[1], rgb[2], &l, &a, &b);
+			labtable[i * 3] = l;
+			labtable[i * 3 + 1] = a;
+			labtable[i * 3 + 2] = b;
+		}
+	}
+	else
+	{
+		colortable = malloc(256 * 3 * sizeof(unsigned char));
+		for (i = 16; i < 256; i ++)
+			xterm2rgb(i, colortable + i * 3);
+	}
 	if (cowheader)
 	{
 		fputs("$the_cow =<<EOC;\n", outfile);
